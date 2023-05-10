8b2c70cbc4387c70d82b38feb064e4851e247468
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-05-10 12:11:39 +0200

    Alex css 3

--- src/pages/Main/Partners/Partners.jsx
+++ src/pages/Main/Partners/Partners.jsx
@@ -4,25 +4,25 @@ import './Partners.css';
 export default function Partners() {
 
   return (
-      <div className='partners container'>
-        <h3 className='partners-title'>НАШІ ПАРТНЕРИ</h3>
+    <div className='partners container'>
+      <h3 className='partners-title'>НАШІ ПАРТНЕРИ</h3>
 
-        <div className='partners-wrapper'>
-          <div className='partners-arrow__picture'>
-            {/* <img className='partners-arrow__img' src="./img/arrow-left.svg" alt="" /> */}
-          </div>
-
-          <div className='partners-map'>
-            <div className='partners-map__item'></div>
-            <div className='partners-map__item'></div>
-            <div className='partners-map__item'></div>
-            <div className='partners-map__item'></div>
-          </div>
-
-          <div className='news-arrow__picture'>
-            {/* <img className='news-arrow__img' src="./img/arrow-right.svg" alt="" /> */}
-          </div>
+      <div className='partners-wrapper'>
+        <div className='news-arrow__picture'>
+          <img className='news-arrow__img' src="./img/arrow-left.svg" alt="" />
         </div>
+
+        <div className='partners-map'>
+          <div className='partners-map__item'></div>
+          <div className='partners-map__item'></div>
+          <div className='partners-map__item'></div>
+          <div className='partners-map__item'></div>
+        </div>
+
+        <div className='news-arrow__picture'>
+          <img className='news-arrow__img' src="./img/arrow-right.svg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
