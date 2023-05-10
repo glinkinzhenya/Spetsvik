04b6a6ed8f7c8829b67290493f464301ead87e3f
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-05-10 12:08:29 +0200

    Alex partners

--- src/pages/Main/News/News.jsx
+++ src/pages/Main/News/News.jsx
@@ -4,46 +4,39 @@ import './News.css';
 export default function News() {
 
   return (
-    <>
-      <div className='news-bg'>
-        <div className='news container'>
-          <h3 className='news-title'>НОВИНИ ТА <span>АКЦІЇ</span></h3>
+    <div className='news-bg'>
+      <div className='news container'>
+        <h3 className='news-title'>НОВИНИ ТА <span>АКЦІЇ</span></h3>
 
-          <div className='news-wrapper'>
-            <div className='news-arrow__picture'>
-              <img className='news-arrow__img' src="./img/arrow-left.svg" alt="" />
+        <div className='news-wrapper'>
+          <div className='news-arrow__picture'>
+            <img className='news-arrow__img' src="./img/arrow-left.svg" alt="" />
+          </div>
+
+          <div className='news-map'>
+            <div className='news-map__item'>
+              <div className='news-map__item-text'>Ціна тижня Берці</div>
+              <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
             </div>
-
-            <div className='news-map'>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
+            <div className='news-map__item'>
+              <div className='news-map__item-text'>Ціна тижня Берці</div>
+              <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
             </div>
-
-            <div className='news-arrow__picture'>
-              <img className='news-arrow__img' src="./img/arrow-right.svg" alt="" />
+            <div className='news-map__item'>
+              <div className='news-map__item-text'>Ціна тижня Берці</div>
+              <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
             </div>
+            <div className='news-map__item'>
+              <div className='news-map__item-text'>Ціна тижня Берці</div>
+              <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
+            </div>
+          </div>
 
+          <div className='news-arrow__picture'>
+            <img className='news-arrow__img' src="./img/arrow-right.svg" alt="" />
           </div>
         </div>
-
-
-
-      </div >
-
-    </>
+      </div>
+    </div >
   );
 }
