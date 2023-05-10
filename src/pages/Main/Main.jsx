6e67a994e856b623939775a1f82025803e95d7e7
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-05-10 09:36:57 +0300

    Alex 2

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Main.css';
 import ImageCarousel from '../ImageCarousel/ImageCarousel';
+import News from '../News/News';
 import CategoriesServises from './CategoriesServises/CategoriesServises';
 
 export default function Main() {
 
   return (
-  
     <main>
       <ImageCarousel />
       <CategoriesServises />
+      <News />
     </main>
   );
 }
