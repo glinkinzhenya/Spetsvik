4acde5dd1527d3ff065482c363ad57c0f310b004
glinkinzhenya <glinkinzhenya@gmail.com>
2023-05-10 14:26:48 +0300

    fvfvfvfv

--- src/pages/Main/News/News.jsx
+++ src/pages/Main/News/News.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './News.css';
 
 export default function News() {
+  const [index, setIndex] = useState(0);
+  const news = [
+    {
+      title: '1',
+      description: 'frfrvfvffr',
+      image: './img/blanket.png'
+    },
+    {
+      title: '2',
+      description: 'frvfvfrfr',
+      image: './img/blanket.png'
+    },
+    {
+      title: '3',
+      description: 'frvfvffrfr',
+      image: './img/blanket.png'
+    },
+    {
+      title: '4',
+      description: 'frfrfvfr',
+      image: './img/blanket.png'
+    },
+    {
+      title: '5',
+      description: 'frfrfvvfr',
+      image: './img/blanket.png'
+    },
+  ];
 
+  const handleClickLeft = () => {
+    const newIndex = index === 0 ? news.length - 1 : index - 1;
+    setIndex(newIndex);
+  };
+
+  const handleClickRight = () => {
+    const newIndex = index === news.length - 1 ? 0 : index + 1;
+    setIndex(newIndex);
+  };
+
+  const newNews = [...news.slice(index, news.length), ...news.slice(0, index)];
+  console.log(newNews);
   return (
     <>
       <div className='news-bg'>
@@ -10,40 +50,29 @@ export default function News() {
           <h3 className='news-title'>НОВИНИ ТА <span>АКЦІЇ</span></h3>
 
           <div className='news-wrapper'>
-            <div className='news-arrow__picture'>
+            <div className='news-arrow__picture' onClick={handleClickLeft}>
               <img className='news-arrow__img' src="./img/arrow-left.svg" alt="" />
             </div>
 
             <div className='news-map'>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
-              <div className='news-map__item'>
-                <div className='news-map__item-text'>Ціна тижня Берці</div>
-                <div className='news-map__item-description'>м. Харків <span>вул. Різдвяна 21</span></div>
-              </div>
+              {newNews.slice(0, 4).map((item, index) => (
+                <div key={index} className='news-map__item'>
+                  <h4 className='news-map__item-title'>{item.title}</h4>
+                  <p className='news-map__item-description'>{item.description}</p>
+                  <div className='news-map__item-picture'>
+                    <img className='news-map__item-image' src={item.image} alt="" />
+                  </div>
+                </div>
+              ))}
             </div>
 
-            <div className='news-arrow__picture'>
+            <div className='news-arrow__picture' onClick={handleClickRight}>
               <img className='news-arrow__img' src="./img/arrow-right.svg" alt="" />
             </div>
 
           </div>
         </div>
-
-
-
       </div >
-
     </>
   );
 }
