cf66c7125fa38e15461d94679cb041cfca6b242d
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-05-12 19:55:51 +0200

    Alex fire 2

--- src/pages/Main/Products/FireSafety/FireSafety.jsx
+++ src/pages/Main/Products/FireSafety/FireSafety.jsx
@@ -5,10 +5,37 @@ export default function FireSafety() {
 
   return (
     <div className='fireSafety'>
-      <div className='services-container container'>
-        <h2 className='services-title'>FireSafety</h2>
+      <div className='boxPath'>
+        <div className='boxPath-container container'>
+          <div className='boxPath-container__text'>Головна</div>
+          <div className='boxPath-container__arrow'>*</div>
+          <div className='boxPath-container__text'>Товари</div>
+          <div className='boxPath-container__arrow'>*</div>
+          <div className='boxPath-container__text'>Пожежна продукція</div>
+        </div>
 
+      </div>
+      <div className='fireSafety-container container'>
+        <h2 className='fireSafety-mainTitle'>ПОЖЕЖНА ПРОДУКЦІЯ</h2>
+
+
+      </div>
+
+      <div className='fireSafety-baner'></div>
+
+      <div className='fireSafety-container container'>
+      <h3 className='fireSafety-secondaryTitle'>Основні категорії</h3>
         
+        <div className='fireSafety-box'>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+          <div className='fireSafety-box__item'></div>
+        </div>
       </div>
     </div>
   );
