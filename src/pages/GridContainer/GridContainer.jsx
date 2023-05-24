435233219b426a5aab8b32d2095afac31b9ee21b
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-05-24 10:50:59 +0300

    Alex header 2

--- src/pages/GridContainer/GridContainer.jsx
+++ src/pages/GridContainer/GridContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+// import { Grid } from '@mui/material';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './GridContainer.css';
@@ -7,17 +7,17 @@ import './GridContainer.css';
 export default function GridContainer({ children }) {
   return (
     <div className='GridTemplate'>
-      <Grid container>
-        <Grid item xs={12} md={12} lg={12}>
+      {/* <Grid container> */}
+        {/* <Grid item xs={12} md={12} lg={12}> */}
           <Header />
-        </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12}>
+        {/* </Grid> */}
+        {/* <Grid item xs={12} sm={12} md={12} lg={12}> */}
           {children}
-        </Grid>
-        <Grid item xs={12} md={12} lg={12}>
+        {/* </Grid> */}
+        {/* // <Grid item xs={12} md={12} lg={12}> */}
           <Footer />
-        </Grid>
-      </Grid>
+        {/* </Grid> */}
+      {/* </Grid> */}
     </div>
   );
 }
