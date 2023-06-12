078b35978c8d874a709553315b26d62516891c62
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-06-12 12:44:01 +0200

    Alex footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -15,7 +15,6 @@ export default function Footer() {
           <div className='footer-left__text'>Спецвік - магазин спецодягу та іншого екіпірування</div>
         </div>
         <div className='footer-catalog'>
-        
           <a className='footer-catalog__item' href="/products">Каталог</a>
           <a className='footer-catalog__item' href="/spets-cloth">Спецодяг</a>
           <a className='footer-catalog__item' href="/shoes">Взуття</a>
@@ -40,21 +39,21 @@ export default function Footer() {
         <div className='footer-right'>
           <div className='footer-right__title'>Адреси магазинів</div>
           <div className='footer-right__item'>
-            <a className='footer-right__item-address' target='_blank' href="https://goo.gl/maps/ci2DPZvv62PQytH9A">м. Запоріжжя, вул. Поштова 141-Е</a>
+            <a className='footer-right__item-address' href="https://goo.gl/maps/ci2DPZvv62PQytH9A" target="_blank" rel="noreferrer">м. Запоріжжя, вул. Поштова 141-Е</a>
             <a className='footer-right__item-telephone' href="tel:+380676120366">
               <img className='footer-right__item-image' src="./img/logo-telephone.svg" alt="" />
               067 612 03 66</a>
           </div>
 
           <div className='footer-right__item'>
-            <a className='footer-right__item-address' target='_blank' href="https://goo.gl/maps/untzH82HCHdjzH4i7">м. Запоріжжя, Бульвар Шевченка 16</a>
+            <a className='footer-right__item-address' href="https://goo.gl/maps/untzH82HCHdjzH4i7" target="_blank" rel="noreferrer">м. Запоріжжя, Бульвар Шевченка 16</a>
             <a className='footer-right__item-telephone' href="tel:+380981807080">
               <img className='footer-right__item-image' src="./img/logo-telephone.svg" alt="" />
               098 180 70 80</a>
           </div>
 
           <div className='footer-right__item'>
-            <a className='footer-right__item-address' target='_blank' href="https://goo.gl/maps/hT7zgvGWuNVq3gEY8">м. Лозова (Харківська область), вул. Богданівська 35
+            <a className='footer-right__item-address' href="https://goo.gl/maps/hT7zgvGWuNVq3gEY8" target="_blank" rel="noreferrer">м. Лозова (Харківська область), вул. Богданівська 35
               (біля магазину СПАР)</a>
             <a className='footer-right__item-telephone' href="tel:+380676132880">
               <img className='footer-right__item-image' src="./img/logo-telephone.svg" alt="" />
@@ -63,7 +62,7 @@ export default function Footer() {
 
           <div className='footer-right__item'>
             <div className='footer-right__item'>
-              <a className='footer-right__item-address' target='_blank' href="https://goo.gl/maps/3bmo4t72UDWPVEzW7">м. Харьків, вул. Різдвяна 21</a>
+              <a className='footer-right__item-address' href="https://goo.gl/maps/3bmo4t72UDWPVEzW7" target="_blank" rel="noreferrer">м. Харьків, вул. Різдвяна 21</a>
               <a className='footer-right__item-telephone' href="tel:+380676121958">
                 <img className='footer-right__item-image' src="./img/logo-telephone.svg" alt="" />
                 067 612 19 58</a>
@@ -72,7 +71,7 @@ export default function Footer() {
 
           <div className='footer-right__item'>
             <div className='footer-right__item'>
-              <a className='footer-right__item-address' target='_blank' href="https://goo.gl/maps/pmMV6SjztNN585nq9">м. Городок (Хмельницька область), вул. Грушевського 84/2</a>
+              <a className='footer-right__item-address' href="https://goo.gl/maps/pmMV6SjztNN585nq9" target="_blank" rel="noreferrer">м. Городок (Хмельницька область), вул. Грушевського 84/2</a>
               <a className='footer-right__item-telephone' href="tel:+380974058748">
                 <img className='footer-right__item-image' src="./img/logo-telephone.svg" alt="" />
                 098 405 87 48</a>
@@ -83,10 +82,10 @@ export default function Footer() {
 
       <div className='footer-secondary'>
         <div className='footer-secondary__social'>
-          <a href='https://www.facebook.com'>
+          <a href='https://www.facebook.com/alexey.mirkvartir' target="_blank" rel="noreferrer">
             <img className='footer-secondary__social-image' src='./img/facebook.png' alt='' />
           </a>
-          <a href='https://www.instagram.com'>
+          <a href='https://www.instagram.com/mir_kvartir_ukraine/' target="_blank" rel="noreferrer">
             <img className='footer-secondary__social-image' src='./img/instagram.png' alt='' />
           </a>
         </div>
