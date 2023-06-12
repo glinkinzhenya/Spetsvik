1e10a3540a4781a283126d4456e1efdd879cf10f
glinkinzhenya <glinkinzhenya@gmail.com>
2023-06-12 16:48:57 +0300

    frfrfrfrfrf

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,6 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAU_cVSiqNEhniG5Hw2bjareHiiB-skRK4',
-//   authDomain: 'spetsvik1-deepdive.firebaseapp.com',
-//   databaseURL: 'https://spetsvik1-deepdive-default-rtdb.europe-west1.firebasedatabase.app',
-//   projectId: 'spetsvik1-deepdive',
-//   storageBucket: 'spetsvik1-deepdive.appspot.com',
-//   messagingSenderId: '669607923989',
-//   appId: '1:669607923989:web:4ee7c3f2bf16dae0dfd91c',
-// };
-
 const firebaseConfig = {
   apiKey: 'AIzaSyCQ81IJtevfAi4ASc9Nnjg66Tf9DTLLrCw',
   authDomain: 'spetsvik-2000.firebaseapp.com',
