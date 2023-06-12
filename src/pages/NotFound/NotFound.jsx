a55f9e8cb2441d13dc62c3886f901942e9d65bec
glinkinzhenya <glinkinzhenya@gmail.com>
2023-06-12 13:16:40 +0300

    jnjnjnjnjjn

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,17 +3,16 @@ import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 export default function NotFound() {
-    return (
-        <div className='not_found'>
-            <button className='not_found_button'>
-                <Link
-                    to={'/'}
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                >Перейти на головну...
-                </Link>
-            </button>
-            
-            <h3 className='not_found-title'>Помилка 404</h3>
-        </div>
-    );
+  return (
+    <div className='not_found'>
+      <button className='not_found_button'>
+        <Link
+          to={'/'}
+          style={{ textDecoration: 'none', color: 'inherit' }}
+        >Перейти на головну...
+        </Link>
+      </button>
+      <h3 className='not_found-title'>Помилка 404</h3>
+    </div>
+  );
 }
